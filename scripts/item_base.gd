b8c84100d4bbaf37f1277591ea0313c14c6b8124
soggy <hello@eriktate.me>
2024-08-21 23:58:44 -0400

    converting all sprite resources to Importality instead of Aseprite wizard

---
+++ scripts/item_base.gd
@@ -0,0 +1,10 @@
+extends Resource
+
+class_name ItemBase
+
+@export var name: String = ""
+@export_multiline var flavor_text: String = ""
+@export var is_crop: bool = false
+@export var is_ingredient: bool = false
+@export var frames: SpriteFrames = null
+
